dc36d5508eeb9fce34c29625e0bc9e990e593515
Hady Salhab <hadysalhab1@gmail.com>
2020-02-25 20:09:16 +1100

    feat: Add models data classes

---
+++ app/src/main/java/com/android/myapplication/newsfeed/models/Articles.kt
@@ -0,0 +1,30 @@
+package com.android.myapplication.newsfeed.models
+
+import com.android.myapplication.newsfeed.repository.SourceRepository
+import com.google.gson.annotations.SerializedName
+
+data class Articles(
+    @SerializedName("author")
+    var author:String?=null,
+    @SerializedName("title")
+    var title:String?=null,
+
+    @SerializedName("description")
+    var description:String?=null,
+
+    @SerializedName("url")
+    var url:String?=null,
+
+    @SerializedName("urlToImage")
+    var urlToImage:String?=null,
+
+    @SerializedName("publishedAt")
+    var publishDate:String?=null,
+
+    @SerializedName("content")
+    var content:String?=null,
+
+    @SerializedName("source")
+    var source:Source
+
+)
