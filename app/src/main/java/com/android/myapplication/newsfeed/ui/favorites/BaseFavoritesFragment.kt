0583835521f088999637dae6833ab43b3b42c8a6
Hady Salhab <hadysalhab1@gmail.com>
2020-02-26 19:25:11 +1100

    feat: Display fragments label in toolbar title

--- app/src/main/java/com/android/myapplication/newsfeed/ui/favorites/BaseFavoritesFragment.kt
+++ app/src/main/java/com/android/myapplication/newsfeed/ui/favorites/BaseFavoritesFragment.kt
@@ -1,7 +1,29 @@
 package com.android.myapplication.newsfeed.ui.favorites
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.android.myapplication.newsfeed.R
 import dagger.android.support.DaggerFragment
 
 abstract class BaseFavoritesFragment : DaggerFragment (){
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupActionBarWithNavController(R.id.favoritesFragment,activity as AppCompatActivity)
+
+    }
+
+    fun setupActionBarWithNavController(fragmentId:Int,activity: AppCompatActivity){
+        val appBarConfiguration = AppBarConfiguration(setOf(fragmentId))
+        NavigationUI.setupActionBarWithNavController(
+            activity,
+            findNavController(),
+            appBarConfiguration
+        )
+    }
 }
