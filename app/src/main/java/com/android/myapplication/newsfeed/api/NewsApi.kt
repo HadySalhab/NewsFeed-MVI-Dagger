1cee76e31ce94b5a904b86ef76c69aa33a0f9c22
Hady Salhab <hadysalhab1@gmail.com>
2020-02-27 17:04:22 +1100

    feat: Add get TopHeadlines request

--- app/src/main/java/com/android/myapplication/newsfeed/api/NewsApi.kt
+++ app/src/main/java/com/android/myapplication/newsfeed/api/NewsApi.kt
@@ -1,4 +1,17 @@
 package com.android.myapplication.newsfeed.api
+
+import android.os.Build
+import com.android.myapplication.newsfeed.BuildConfig
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface NewsApi {
+    @GET("v2/top-headlines")
+    fun getTopHeadlines(
+        @Query("country") country:String,
+        @Query("category") category: String = "general",
+        @Query("page") page:Int = 1,
+        @Query("apiKey") apiKey:String = BuildConfig.API_KEY
+    )
 
 }
