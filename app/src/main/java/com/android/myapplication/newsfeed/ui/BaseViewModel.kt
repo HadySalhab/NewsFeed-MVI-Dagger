233e45df04fae72d9fbdf2b2326efdd9c588f8cf
Hady Salhab <hadysalhab1@gmail.com>
2020-02-27 16:16:57 +1100

    feat: Add BaseViewModel

--- app/src/main/java/com/android/myapplication/newsfeed/ui/BaseViewModel.kt
+++ app/src/main/java/com/android/myapplication/newsfeed/ui/BaseViewModel.kt
@@ -17,12 +17,12 @@ abstract class BaseViewModel<StateEvent, ViewState> : ViewModel()
     val viewState: LiveData<ViewState>
         get() = _viewState
 
-  /*  val dataState: LiveData<DataState<ViewState>> = Transformations
+    val dataState: LiveData<DataState<ViewState>> = Transformations
         .switchMap(_stateEvent){stateEvent ->
             stateEvent?.let {
                 handleStateEvent(stateEvent)
             }
-        }*/
+        }
 
     fun setStateEvent(event: StateEvent){
         _stateEvent.value = event
@@ -35,7 +35,7 @@ abstract class BaseViewModel<StateEvent, ViewState> : ViewModel()
         return value
     }
 
-    /*abstract fun handleStateEvent(stateEvent: StateEvent): LiveData<DataState<ViewState>>*/
+    abstract fun handleStateEvent(stateEvent: StateEvent): LiveData<DataState<ViewState>>
 
     abstract fun initNewViewState(): ViewState
 
