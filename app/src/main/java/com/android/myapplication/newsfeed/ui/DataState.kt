5e1d7d0f18b8fbc7e1e4018ddb5833e90031ca43
Hady Salhab <hadysalhab1@gmail.com>
2020-02-27 17:12:45 +1100

    feat: Implement DataState data class

--- app/src/main/java/com/android/myapplication/newsfeed/ui/DataState.kt
+++ app/src/main/java/com/android/myapplication/newsfeed/ui/DataState.kt
@@ -1,6 +1,56 @@
 package com.android.myapplication.newsfeed.ui
 
- class DataState <T>
-{
+import com.android.myapplication.newsfeed.ui.sources.*
 
+data class DataState<T>(
+ var error: Event<StateError>? = null,
+ var loading: Loading = Loading(false),
+ var data: Data<T>? = null
+) {
+
+ companion object {
+
+  fun <T> error(
+   response: Response
+  ): DataState<T> {
+   return DataState(
+    error = Event(
+     StateError(
+      response
+     )
+    ),
+    loading = Loading(false),
+    data = null
+   )
+  }
+
+  fun <T> loading(
+   isLoading: Boolean,
+   cachedData: T? = null
+  ): DataState<T> {
+   return DataState(
+    error = null,
+    loading = Loading(isLoading),
+    data = Data(
+     Event.dataEvent(
+      cachedData
+     ), null
+    )
+   )
+  }
+
+  fun <T> data(
+   data: T? = null,
+   response: Response? = null
+  ): DataState<T> {
+   return DataState(
+    error = null,
+    loading = Loading(false),
+    data = Data(
+     Event.dataEvent(data),
+     Event.responseEvent(response)
+    )
+   )
+  }
+ }
 }
