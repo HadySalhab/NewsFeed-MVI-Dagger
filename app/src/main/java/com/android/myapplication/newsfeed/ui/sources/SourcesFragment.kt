0e1fc95fb2d94c8b34173b09850ba1ecaa06fedd
Hady Salhab <hadysalhab1@gmail.com>
2020-03-02 17:44:28 +1100

    feat: Add Viewpager to SourcesFragment

--- app/src/main/java/com/android/myapplication/newsfeed/ui/sources/SourcesFragment.kt
+++ app/src/main/java/com/android/myapplication/newsfeed/ui/sources/SourcesFragment.kt
@@ -5,25 +5,61 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.android.myapplication.newsfeed.R
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.fragment_sources.*
 
 class SourcesFragment : BaseSourcesFragment (){
+    private lateinit var viewPager:ViewPager2
+    private lateinit var tabLayout: TabLayout
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-
-        return inflater.inflate(R.layout.fragment_sources,container,false)
+        val view = inflater.inflate(R.layout.fragment_sources,container,false)
+        viewPager = view.findViewById(R.id.viewpager)
+        tabLayout = view.findViewById(R.id.tabs)
+        initViewPager()
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
 
-        btn_nav.setOnClickListener {
-            findNavController().navigate(R.id.action_sourcesFragment_to_articlesSourceFragment)
+    private fun initViewPager(){
+        viewPager.adapter = SourcesPagerAdapter(this)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = getTabTitle(position)
+        }.attach()
+    }
+ /*   private fun getTabIcon(position: Int): Int {
+        return when (position) {
+            GENERAL_SOURCE_PAGE_INDEX -> R.drawable.garden_tab_selector
+            BUSINESS_SOURCE_PAGE_INDEX -> R.drawable.plant_list_tab_selector
+            ENTERTAINMENT_SOURCE_PAGE_INDEX -> R.drawable.garden_tab_selector
+            HEALTH_SOURCE_PAGE_INDEX -> R.drawable.plant_list_tab_selector
+            SCIENCE_SOURCE_PAGE_INDEX -> R.drawable.garden_tab_selector
+            SPORTS_SOURCE_PAGE_INDEX -> R.drawable.plant_list_tab_selector
+            TECHNOLOGY_SOURCE_PAGE_INDEX -> R.drawable.garden_tab_selector
+            else -> throw IndexOutOfBoundsException()
+        }
+    }*/
+
+    private fun getTabTitle(position: Int): String? {
+        return when (position) {
+            GENERAL_SOURCE_PAGE_INDEX -> getString(R.string.tab_general)
+            BUSINESS_SOURCE_PAGE_INDEX -> getString(R.string.tab_business)
+            ENTERTAINMENT_SOURCE_PAGE_INDEX -> getString(R.string.tab_entertainment)
+            HEALTH_SOURCE_PAGE_INDEX -> getString(R.string.tab_health)
+            SCIENCE_SOURCE_PAGE_INDEX -> getString(R.string.tab_science)
+            SPORTS_SOURCE_PAGE_INDEX -> getString(R.string.tab_sports)
+            TECHNOLOGY_SOURCE_PAGE_INDEX -> getString(R.string.tab_technology)
+            else -> null
         }
     }
 }
---
+++ app/src/main/java/com/android/myapplication/newsfeed/ui/sources/SourcesPagerAdapter.kt
@@ -0,0 +1,36 @@
+package com.android.myapplication.newsfeed.ui.sources
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.android.myapplication.newsfeed.ui.sources.categories.*
+
+const val GENERAL_SOURCE_PAGE_INDEX = 0
+const val BUSINESS_SOURCE_PAGE_INDEX = 1
+const val ENTERTAINMENT_SOURCE_PAGE_INDEX = 2
+const val HEALTH_SOURCE_PAGE_INDEX = 3
+const val SCIENCE_SOURCE_PAGE_INDEX = 4
+const val SPORTS_SOURCE_PAGE_INDEX = 5
+const val TECHNOLOGY_SOURCE_PAGE_INDEX = 6
+
+
+class SourcesPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    /**
+     * Mapping of the ViewPager page indexes to their respective Fragments
+     */
+    private val tabFragmentsCreators: Map<Int, () -> Fragment> = mapOf(
+        GENERAL_SOURCE_PAGE_INDEX to { GeneralFragment() },
+        BUSINESS_SOURCE_PAGE_INDEX to { BusinessFragment() },
+        ENTERTAINMENT_SOURCE_PAGE_INDEX to { EntertainmentFragment() },
+        HEALTH_SOURCE_PAGE_INDEX to { HealthFragment() },
+        SCIENCE_SOURCE_PAGE_INDEX to { ScienceFragment() },
+        SPORTS_SOURCE_PAGE_INDEX to { SportFragment() },
+        TECHNOLOGY_SOURCE_PAGE_INDEX to { TechnologyFragment() }
+    )
+
+    override fun getItemCount() = tabFragmentsCreators.size
+
+    override fun createFragment(position: Int): Fragment {
+        return tabFragmentsCreators[position]?.invoke() ?: throw IndexOutOfBoundsException()
+    }
+}
