b13ce0bdebb69da8b3aa177bfb5c9dd406adcfa7
Hady Salhab <hadysalhab1@gmail.com>
2020-03-02 12:19:27 +1100

    feat: set default value for country in getTopHeadlines() request

--- app/src/main/java/com/android/myapplication/newsfeed/api/NewsApi.kt
+++ app/src/main/java/com/android/myapplication/newsfeed/api/NewsApi.kt
@@ -10,7 +10,7 @@ import retrofit2.http.Query
 interface NewsApi {
     @GET("v2/top-headlines")
     fun getTopHeadlines(
-        @Query("country") country:String,
+        @Query("country") country:String?="",
         @Query("category") category: String = "general",
         @Query("page") page:Int = 1,
         @Query("apiKey") apiKey:String = BuildConfig.API_KEY
