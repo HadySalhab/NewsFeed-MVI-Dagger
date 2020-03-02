38ceb48d322627506bc7429f2cd58956c1bccc2d
Hady Salhab <hadysalhab1@gmail.com>
2020-03-02 18:33:25 +1100

    fix: Handle Headline Request Event Config change bug

--- app/src/main/java/com/android/myapplication/newsfeed/ui/headlines/HeadlineFragment.kt
+++ app/src/main/java/com/android/myapplication/newsfeed/ui/headlines/HeadlineFragment.kt
@@ -7,9 +7,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AbsListView
 import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.myapplication.newsfeed.R
@@ -17,45 +15,61 @@ import com.android.myapplication.newsfeed.models.Article
 import com.android.myapplication.newsfeed.ui.headlines.state.HeadlinesStateEvent
 import com.bumptech.glide.RequestManager
 import kotlinx.android.synthetic.main.fragment_headlines.*
-import kotlinx.android.synthetic.main.fragment_sources.*
 import javax.inject.Inject
 
-class HeadlineFragment  : BaseHeadlineFragment(),HeadlinesListAdapter.Interaction{
+class HeadlineFragment : BaseHeadlineFragment(), HeadlinesListAdapter.Interaction {
 
     @Inject
     lateinit var requestManager: RequestManager
 
-    private lateinit var headlinesAdapter:HeadlinesListAdapter
-    private lateinit var rv:RecyclerView
+    private lateinit var headlinesAdapter: HeadlinesListAdapter
+    private lateinit var rv: RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        val view = inflater.inflate(R.layout.fragment_headlines,container,false)
+        val view = inflater.inflate(R.layout.fragment_headlines, container, false)
         rv = view.findViewById(R.id.rv_headlines)
         initRV()
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState) //BaseHeadlineFragment implementation and Fragment()
+        super.onViewCreated(
+            view,
+            savedInstanceState
+        ) //BaseHeadlineFragment implementation and Fragment()
         subscribeObservers()
         executeRequest()
     }
-    private fun executeRequest(){
-        viewModel.setStateEvent(HeadlinesStateEvent.HeadlinesSearchEvent())
+
+    private fun executeRequest() {
+        // no point of firing the event everytime we rotate or change graph
+        //as long as the viewModel is alive, no need to re-fire
+        //viewModel life span is tied to the MainActivity (store)
+        //  queryEvent will be held in the viewModel and set to true when the viewModel is first created
+        // when we rotate , the queryEvent liveData will still hold the same object/event, which is already been handled
+        // NOTE: even though the DataState member variables are wrapped in event,
+        // it will be refreshed because its event object are being updated, in the networkBoundResources and repositories every time we fire this request
+        viewModel.executeQueryEvent.observe(viewLifecycleOwner, Observer { queryEvent->
+                queryEvent.getContentIfNotHandled()?.let { //only proceed if this query has never been handled
+                    viewModel.setStateEvent(HeadlinesStateEvent.HeadlinesSearchEvent())
+                }
+            })
+
+
     }
 
-    private fun subscribeObservers(){
-        viewModel.dataState.observe(viewLifecycleOwner, Observer { dataState->
+    private fun subscribeObservers() {
+        viewModel.dataState.observe(viewLifecycleOwner, Observer { dataState ->
             dataState?.let {
                 stateChangeListener.onDataStateChange(dataState) //let the listener invoke their onDataStateChange impl (error, loading ,data response)
                 //this component handle the data data
                 dataState.data?.let {
                     it.data?.let { event ->
-                        event.getContentIfNotHandled()?.let {viewState->
+                        event.getContentIfNotHandled()?.let { viewState ->
                             Log.d(TAG, "HeadlineFragment: viewState: $viewState")
                             //we are updating a field in the viewState, which will update the viewState itself
                             // and fire observers
@@ -65,25 +79,30 @@ class HeadlineFragment  : BaseHeadlineFragment(),HeadlinesListAdapter.Interactio
                 }
             }
         })
-        viewModel.viewState.observe(viewLifecycleOwner, Observer { viewState->
+        //As soon as the HeadlineFrag is created , it will receive the viewState (if available) in the ViewModel
+        // and everTime the viewState is changed
+        viewModel.viewState.observe(viewLifecycleOwner, Observer { viewState ->
             Log.d(TAG, "HeadlineFragment: viewState: $viewState")
             viewState?.let {
-                headlinesAdapter.submitList(list=it.headlinesFields.headlinesList,isQueryExhausted = true)
+                headlinesAdapter.submitList(
+                    list = it.headlinesFields.headlinesList,
+                    isQueryExhausted = true
+                )
             }
         })
     }
 
-    private fun initRV(){
+    private fun initRV() {
         rv.apply {
             layoutManager = LinearLayoutManager(this@HeadlineFragment.context)
-            headlinesAdapter = HeadlinesListAdapter(this@HeadlineFragment,requestManager)
+            headlinesAdapter = HeadlinesListAdapter(this@HeadlineFragment, requestManager)
 
-            addOnScrollListener(object :RecyclerView.OnScrollListener(){
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
                 override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                     super.onScrollStateChanged(recyclerView, newState)
                     val layoutManager = recyclerView.layoutManager as LinearLayoutManager
                     val lastPosition = layoutManager.findLastVisibleItemPosition()
-                    if(lastPosition==headlinesAdapter.itemCount.minus(1)){
+                    if (lastPosition == headlinesAdapter.itemCount.minus(1)) {
                         Log.d(TAG, "HeadlineFragment: load next page...")
                     }
                 }
@@ -101,7 +120,8 @@ class HeadlineFragment  : BaseHeadlineFragment(),HeadlinesListAdapter.Interactio
         Log.d(TAG, "onItemSelected: position,article: ${position}, ${item} ")
         fireIntent(item)
     }
-    private fun fireIntent(item: Article){
+
+    private fun fireIntent(item: Article) {
         val url = item.url
         val intent = Intent(Intent.ACTION_VIEW)
         intent.data = Uri.parse(url)
--- app/src/main/java/com/android/myapplication/newsfeed/ui/headlines/HeadlinesViewModel.kt
+++ app/src/main/java/com/android/myapplication/newsfeed/ui/headlines/HeadlinesViewModel.kt
@@ -2,12 +2,14 @@ package com.android.myapplication.newsfeed.ui.headlines
 
 import android.content.SharedPreferences
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.android.myapplication.newsfeed.models.Article
 import com.android.myapplication.newsfeed.repository.HeadlinesRepository
 import com.android.myapplication.newsfeed.ui.BaseViewModel
 import com.android.myapplication.newsfeed.ui.DataState
 import com.android.myapplication.newsfeed.ui.headlines.state.HeadlinesStateEvent
 import com.android.myapplication.newsfeed.ui.headlines.state.HeadlinesViewState
+import com.android.myapplication.newsfeed.ui.Event
 import com.android.myapplication.newsfeed.util.AbsentLiveData
 import com.bumptech.glide.RequestManager
 import javax.inject.Inject
@@ -20,47 +22,55 @@ constructor(
     private val sharedPreferences: SharedPreferences,
     private val requestManager: RequestManager
 
-) : BaseViewModel<HeadlinesStateEvent,HeadlinesViewState>()
-{
+) : BaseViewModel<HeadlinesStateEvent, HeadlinesViewState>() {
+
+    private val _executeQueryEvent = MutableLiveData<Event<Boolean>>(
+        Event(true)
+    ) //this event will set to true when this viewModel is first created
+    val executeQueryEvent: LiveData<Event<Boolean>>
+        get() = _executeQueryEvent
+
 
     override fun initNewViewState(): HeadlinesViewState {
         return HeadlinesViewState()
     }
 
     override fun handleStateEvent(stateEvent: HeadlinesStateEvent): LiveData<DataState<HeadlinesViewState>> {
-        when(stateEvent){
-            is HeadlinesStateEvent.HeadlinesSearchEvent->{
+        when (stateEvent) {
+            is HeadlinesStateEvent.HeadlinesSearchEvent -> {
                 return headlinesRepository.getTopHeadlines(
-                   stateEvent.country,
+                    stateEvent.country,
                     stateEvent.category
                 )
             }
-            is HeadlinesStateEvent.None->{
+            is HeadlinesStateEvent.None -> {
                 return AbsentLiveData.create()
             }
         }
     }
 
-    fun setQuery(query:String){
+    fun setQuery(query: String) {
         val update = getCurrentViewStateOrNew()
-        if(query.equals(update.headlinesFields.searchQuery)){
+        if (query.equals(update.headlinesFields.searchQuery)) {
             return
         }
         update.headlinesFields.searchQuery = query
         _viewState.value = update
     }
-    fun setHeadlineListData(headlinesList:List<Article>){
+
+    fun setHeadlineListData(headlinesList: List<Article>) {
         val update = getCurrentViewStateOrNew()
         //we are not checking if it's the same list being passed, because RecycleView DiffUtil will take care of it
         update.headlinesFields.headlinesList = headlinesList
         _viewState.value = update
     }
 
-     fun cancelActiveJobs(){
+    fun cancelActiveJobs() {
         headlinesRepository.cancelActiveJobs() //repository extends JobManager, cancelActiveJobs is part of the job Manager
         handlePendingData()
     }
-    private fun handlePendingData(){
+
+    private fun handlePendingData() {
         setStateEvent(HeadlinesStateEvent.None())
     }
 
@@ -68,6 +78,5 @@ constructor(
         super.onCleared()
         cancelActiveJobs()
     }
-
 
 }
