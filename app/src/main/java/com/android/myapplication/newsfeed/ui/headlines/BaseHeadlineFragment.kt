a3ee05f222ab072ce9193762bed06a9693843031
Hady Salhab <hadysalhab1@gmail.com>
2020-03-02 12:34:18 +1100

    feat: Add stateChangeListener field to BaseHeadlineFragment

--- app/src/main/java/com/android/myapplication/newsfeed/ui/headlines/BaseHeadlineFragment.kt
+++ app/src/main/java/com/android/myapplication/newsfeed/ui/headlines/BaseHeadlineFragment.kt
@@ -1,6 +1,8 @@
 package com.android.myapplication.newsfeed.ui.headlines
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -8,6 +10,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.android.myapplication.newsfeed.R
+import com.android.myapplication.newsfeed.ui.DataStateChangeListener
 import com.android.myapplication.newsfeed.viewmodels.ViewModelProviderFactory
 import dagger.android.support.DaggerFragment
 import java.lang.Exception
@@ -22,7 +25,7 @@ abstract class BaseHeadlineFragment : DaggerFragment (){
 
     lateinit var viewModel: HeadlinesViewModel
 
-
+    lateinit var stateChangeListener: DataStateChangeListener
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -48,5 +51,14 @@ abstract class BaseHeadlineFragment : DaggerFragment (){
             findNavController(),
             appBarConfiguration
         )
+    }
+    //getting the activity
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try{
+            stateChangeListener = context as DataStateChangeListener
+        }catch(e: ClassCastException){
+            Log.e(TAG, "$context must implement DataStateChangeListener" )
+        }
     }
 }
