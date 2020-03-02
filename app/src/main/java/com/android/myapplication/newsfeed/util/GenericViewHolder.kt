d3cc50ab6b67fdcf11d9efc52b4cd7e23e72ba25
Hady Salhab <hadysalhab1@gmail.com>
2020-03-02 14:36:25 +1100

    feat: Add GenericViewHolder class

---
+++ app/src/main/java/com/android/myapplication/newsfeed/util/GenericViewHolder.kt
@@ -0,0 +1,7 @@
+package com.android.myapplication.newsfeed.util
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class GenericViewHolder
+constructor(itemView:View): RecyclerView.ViewHolder(itemView)
