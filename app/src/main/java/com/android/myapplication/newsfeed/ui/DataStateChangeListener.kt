40a0d591bbff90aea091e93b94d347c677369fb8
Hady Salhab <hadysalhab1@gmail.com>
2020-03-02 12:35:21 +1100

    feat: Update DataStateChangeListener

--- app/src/main/java/com/android/myapplication/newsfeed/ui/DataStateChangeListener.kt
+++ app/src/main/java/com/android/myapplication/newsfeed/ui/DataStateChangeListener.kt
@@ -2,4 +2,6 @@ package com.android.myapplication.newsfeed.ui
 
 interface DataStateChangeListener {
     fun expandAppBar()
+    fun onDataStateChange(dataState: DataState<*>?)
+    fun hideSoftKeyboard()
 }
