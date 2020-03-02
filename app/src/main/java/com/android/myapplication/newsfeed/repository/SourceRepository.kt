a9c0b595b8dd6dbea1d1143592aeebf21bb50b50
Hady Salhab <hadysalhab1@gmail.com>
2020-03-02 23:47:07 +1100

    feat: Implement SourceRepository

--- app/src/main/java/com/android/myapplication/newsfeed/repository/SourceRepository.kt
+++ app/src/main/java/com/android/myapplication/newsfeed/repository/SourceRepository.kt
@@ -1,8 +1,78 @@
 package com.android.myapplication.newsfeed.repository
 
+import android.util.Log
+import androidx.lifecycle.LiveData
 import com.android.myapplication.newsfeed.api.NewsApi
+import com.android.myapplication.newsfeed.api.data.ArticleNetwork
+import com.android.myapplication.newsfeed.api.data.SourceNetwork
+import com.android.myapplication.newsfeed.api.responses.SourcesResponse
+import com.android.myapplication.newsfeed.models.Article
+import com.android.myapplication.newsfeed.models.Source
+import com.android.myapplication.newsfeed.persistence.ArticleDb
+import com.android.myapplication.newsfeed.ui.DataState
+import com.android.myapplication.newsfeed.ui.headlines.state.HeadlinesViewState
+import com.android.myapplication.newsfeed.ui.sources.state.SourcesViewState
+import com.android.myapplication.newsfeed.util.ApiSuccessResponse
+import com.android.myapplication.newsfeed.util.GenericApiResponse
+import com.android.myapplication.newsfeed.util.NetworkUtil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.withContext
 
 class SourceRepository
 constructor(
-    val newsApi: NewsApi
-)
+    val newsApi: NewsApi,
+    val networkUtil: NetworkUtil
+): JobManager("HeadlinesRepository") {
+    private val TAG: String = "AppDebug"
+
+    fun getSources(): LiveData<DataState<SourcesViewState>> {
+        return object :NetworkBoundResource<SourcesResponse,Void,SourcesViewState>(networkUtil.isConnectedToTheInternet(),false){
+            override fun setJob(job: Job) {
+                addJob("getSources", job)
+            }
+
+            override suspend fun createCacheRequestAndReturn() {
+                //N/A
+            }
+
+            override suspend fun handleApiSuccessResponse(response: ApiSuccessResponse<SourcesResponse>) {
+                val sourceList: ArrayList<Source> = ArrayList()
+                //handleApiSuccessResponse is already called inside a coroutine with IO dispatcher
+                val sourcesNetwork :List<SourceNetwork>? = response.body.sourcesNetwork
+
+
+                sourcesNetwork?.forEach { sourceNetwork->
+                    sourceList.add(
+                           Source(
+                               sourceNetwork.id,
+                               sourceNetwork.name,
+                               sourceNetwork.description,
+                               sourceNetwork.url,
+                               sourceNetwork.country,
+                               sourceNetwork.language,
+                               sourceNetwork.category
+                           )
+                        )
+                    }
+
+                //switch context because handleApiSuccessResponse is running inside IO dispatcher
+                withContext(Dispatchers.Main){
+                    onCompleteJob(DataState.data(data = SourcesViewState(
+                        sourcesField = SourcesViewState.SourcesField(sourceList)
+                    )))
+                }
+
+            }
+
+            override fun createCall(): LiveData<GenericApiResponse<SourcesResponse>> {
+                return newsApi.getSources()
+            }
+
+            override fun loadFromCache(): Void? {
+                return null
+            }
+
+        } .asLiveData()
+    }
+}
