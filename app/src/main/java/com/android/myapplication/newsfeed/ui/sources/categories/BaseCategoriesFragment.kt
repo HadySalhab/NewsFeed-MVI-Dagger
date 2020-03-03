d92de84924711d54d67876e0dd6d918d82d7cd85
Hady Salhab <hadysalhab1@gmail.com>
2020-03-03 16:13:14 +1100

    feat: Create BaseCategoriesFragment

---
+++ app/src/main/java/com/android/myapplication/newsfeed/ui/sources/categories/BaseCategoriesFragment.kt
@@ -0,0 +1,67 @@
+package com.android.myapplication.newsfeed.ui.sources.categories
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.myapplication.newsfeed.R
+import com.android.myapplication.newsfeed.models.Source
+import com.android.myapplication.newsfeed.ui.sources.BaseCategoriesSourcesFragment
+
+abstract class BaseCategoriesFragment : BaseCategoriesSourcesFragment(),
+    SourceListAdapter.Interaction {
+    private var recyclerView: RecyclerView? = null
+    private lateinit var sourceListAdapter: SourceListAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflateView(inflater, container)
+        recyclerView = findRV(view)
+        initRV()
+        return view
+    }
+
+    abstract fun inflateView(inflater: LayoutInflater, container: ViewGroup?):View
+    abstract fun findRV(view:View): RecyclerView
+
+
+    override fun onItemSelected(position: Int, item: Source) {
+        Log.d(TAG, "GeneralFragment: source list item selected: ${item} ")
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, "BaseCategoriesFragment: onViewCreated: ${viewModel}")
+        subscribeObservers()
+        //the request has already been executed by the source fragment
+    }
+
+    private fun initRV() {
+        recyclerView!!.apply {
+            layoutManager = LinearLayoutManager(this@BaseCategoriesFragment.context)
+            sourceListAdapter = SourceListAdapter(this@BaseCategoriesFragment)
+            adapter = sourceListAdapter
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        recyclerView!!.adapter = null //to avoid memory leak
+    }
+    private fun subscribeObservers() {
+        viewModel.viewState.observe(viewLifecycleOwner, Observer { viewModelViewState ->
+            viewModelViewState?.let {
+                val sourceList = it.sourcesField.sourceList
+                val generalSourceList = filterSourceList(sourceList)
+                sourceListAdapter.submitList(generalSourceList)
+            }
+        })
+    }
+    abstract fun filterSourceList(sourceList:List<Source>):List<Source>
+
+}
