babe06bd36e467f3927f224ac27e1e9e38fdb097
Hady Salhab <hadysalhab1@gmail.com>
2020-03-03 15:52:55 +1100

    feat: Implement SourceFragment

--- app/src/main/java/com/android/myapplication/newsfeed/ui/sources/SourcesFragment.kt
+++ app/src/main/java/com/android/myapplication/newsfeed/ui/sources/SourcesFragment.kt
@@ -5,8 +5,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.viewpager2.widget.ViewPager2
 import com.android.myapplication.newsfeed.R
+import com.android.myapplication.newsfeed.ui.sources.state.SourcesStateEvent
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -15,6 +17,7 @@ class SourcesFragment : BaseSourcesFragment() {
     private var tabLayout: TabLayout? = null
     private var tabLayoutMediator:TabLayoutMediator?=null
     private var adapter:SourcesPagerAdapter?=null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -31,6 +34,43 @@ class SourcesFragment : BaseSourcesFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.d(TAG, "SourcesFragment: onViewCreated: ${viewModel} ")
+        subscribeObservers()
+        executeRequest()
+    }
+
+    private fun executeRequest(){
+        viewModel.executeQueryEvent.observe(viewLifecycleOwner, Observer { queryEvent->
+            //we dont want the query event or request to be re-fired when the config change happens
+            // queryEvent life is attached to the viewModel which has the life of the activity (until its finished)
+            queryEvent.getContentIfNotHandled()?.let {
+                Log.d(TAG, "SourcesFragment: executeRequest: $queryEvent")
+                viewModel.setStateEvent(SourcesStateEvent.SourcesSearchEvent())
+            }
+
+        })
+    }
+    private fun subscribeObservers() {
+        //SourceFragment is responsible of updating the viewState in the viewModel when the dataState returns
+        viewModel.dataState.observe(viewLifecycleOwner, Observer { dataState->
+            dataState?.let {
+                stateChangeListener?.onDataStateChange(dataState) //Listener(BaseActivity/Activity) will handle the loading, error dialog/toast, data response msg
+                dataState.data?.let {
+                    it.data?.let { eventViewState->
+                        eventViewState.getContentIfNotHandled()?.let { networkViewState->
+                            Log.d(TAG, "SourcesFragment: dataStateReturned: with data!=null")
+                            viewModel.setSourceListData(networkViewState.sourcesField.sourceList)
+                        }
+                    }
+                }
+                it.error?.let{ errorEvent->
+                    errorEvent.getContentIfNotHandled()?.let { stateError ->
+                        viewModel.setErrorScreenMsg(stateError.response.message?:"")
+                    }
+                }
+            }
+        })
+
+        //Observing the ViewState is the role of categories fragment
     }
 
     private fun initViewPager() {
