c668cd3050151713bde8aeb8bc8a7b804195192f
Hady Salhab <hadysalhab1@gmail.com>
2020-03-03 16:19:18 +1100

    feat: Create SourcesCategories constants

---
+++ app/src/main/java/com/android/myapplication/newsfeed/util/SourcesCategories.kt
@@ -0,0 +1,14 @@
+package com.android.myapplication.newsfeed.util
+
+class SourcesCategories {
+    companion object {
+        const val BUSINESS = "business"
+        const val ENTERTAINMENT = "entertainment"
+        const val GENERAL = "general"
+        const val HEALTH = "health"
+        const val SCIENCE = "science"
+        const val SPORTS = "sports"
+        const val TECHNOLOGY = "technology"
+
+    }
+}
