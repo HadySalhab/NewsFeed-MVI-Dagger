64a911e1b7077655fe084076fbf517528dbca4f2
Hady Salhab <hadysalhab1@gmail.com>
2020-03-10 11:48:51 +1100

    refactor: Use kotlin top-level constants

--- app/src/main/java/com/android/myapplication/newsfeed/util/Constants.kt
+++ app/src/main/java/com/android/myapplication/newsfeed/util/Constants.kt
@@ -1,10 +1,7 @@
 package com.android.myapplication.newsfeed.util
 
-class Constants {
-    companion object{
-        val BASE_URL = "http://newsapi.org/"
-        const val NETWORK_TIMEOUT = 6000L
-        const val TESTING_NETWORK_DELAY = 0L // fake network delay for testing
-        const val TESTING_CACHE_DELAY = 0L // fake cache delay for testing
-    }
-}
+
+    const val BASE_URL = "http://newsapi.org/"
+    const val NETWORK_TIMEOUT = 6000L
+    const val TESTING_NETWORK_DELAY = 0L // fake network delay for testing
+    const val TESTING_CACHE_DELAY = 0L // fake cache delay for testing
